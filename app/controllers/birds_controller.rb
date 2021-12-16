0b0c5a9ff90f31edf9677b9edbe1ff19be80f84e
dialloflatno <brahimdiallo23@gmail.com>
2021-12-16 01:17:58 -0500

    commit tree ff3eec3905997fd3e75b9ebefbd1541074115218

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -8,7 +8,8 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(params.permit(:name,:species))
+    # bird = Bird.create(name: params[:name], species: params[:species])
     render json: bird, status: :created
   end
 
